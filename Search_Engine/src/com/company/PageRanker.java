74e8a27370cb0e190ff9d9c375786518c08ff8c0
AhmadNaderAdel <ahmadnaderx1998@gmail.com>
2020-05-23 22:29:48 +0200

    Finished Popularity Ranker

--- Search_Engine/src/com/company/PageRanker.java
+++ Search_Engine/src/com/company/PageRanker.java
@@ -22,43 +22,57 @@ public class PageRanker  {
 
     public static void calculatePageRank(MongoDatabase database){
         MongoCollection<Document> collection = database.getCollection(CRAWLER_COLLECTION_NAME);
-        MongoCursor<Document> cur = collection.find().cursor();
+        long noOfDocuments = collection.countDocuments();
+        System.out.println("No Of Documents is " + noOfDocuments);
+        // collection.update
+        BasicDBObject resetQuery = new BasicDBObject();
+        //query.put("$exists", true); // (1)
 
-        if(cur.hasNext()) {
-            Document current = cur.next();
-            String currentUrl = (String) current.get("url");
-            ArrayList<String> courses = (ArrayList<String>) current.get("InboundLinks");
-            ArrayList<URLRank>  pageRankArray = new ArrayList<URLRank>();
-            MongoCursor<Document> curt = collection.find().cursor();
-            while(curt.hasNext()) {
+        BasicDBObject resetDocument = new BasicDBObject();
+        resetDocument.put("PageRank", ((float) 1/noOfDocuments)); // (2)
+
+        BasicDBObject resetObject = new BasicDBObject();
+        resetObject.put("$set", resetDocument); // (3)
+        collection.updateMany(resetQuery, resetObject);
+        double dampingFactor = 0.85;
+        for (int i = 0; i < noOfDocuments - 1; i++) {
+            MongoCursor<Document> cur = collection.find().cursor();
+            while (cur.hasNext()) {
+                Document current = cur.next();
+                ArrayList<String> courses = (ArrayList<String>) current.get("InboundLinks");
+                ArrayList<URLRank> pageRankArray = new ArrayList<URLRank>();
+                MongoCursor<Document> curt = collection.find().cursor();
+                while (curt.hasNext()) {
                     Document doc = curt.next();
-
                     String s = (String) doc.get("url");
-                    for (String e: courses){
-                        if (e.equals(s)){
+                    for (String e : courses) {
+                        if (e.equals(s)) {
                             double rank = (double) doc.get("PageRank");
                             int noOfLinks = (int) doc.get("NoOfOutBoundLinks");
                             pageRankArray.add(new URLRank(rank, noOfLinks));
+                            break;
                         }
                     }
+                }
+                double actualRank = 0;
+                for (URLRank e : pageRankArray) {
+                    actualRank += e.calculateRankSlice();
+                }
+                actualRank = (1-dampingFactor) + dampingFactor*actualRank;
+                System.out.println(actualRank);
+                String currentUrl = (String) current.get("url");
+                BasicDBObject query = new BasicDBObject();
+                query.put("url", currentUrl); // (1)
+
+                BasicDBObject newDocument = new BasicDBObject();
+                newDocument.put("PageRank", actualRank); // (2)
+
+                BasicDBObject updateObject = new BasicDBObject();
+                updateObject.put("$set", newDocument); // (3)
+
+                database.getCollection("Index").updateMany(query, updateObject);
             }
-            double actualRank = 0;
-            for (URLRank e: pageRankArray) {
-                actualRank += e.calculateRankSlice();
-            }
-            System.out.println(actualRank);
-            BasicDBObject query = new BasicDBObject();
-            query.put("url", currentUrl); // (1)
-
-            BasicDBObject newDocument = new BasicDBObject();
-            newDocument.put("PageRank", actualRank); // (2)
-
-            BasicDBObject updateObject = new BasicDBObject();
-            updateObject.put("$set", newDocument); // (3)
-
-            database.getCollection("Index").updateMany(query, updateObject);
         }
-
     }
 
 
