d530841c86e873f4b27f19a7fc7effecafde5dbf
KamelMohsen <kamelmohsenkamel@gmail.com>
2020-06-03 23:42:59 +0200

    Done till images Search

--- QueryProcessor/src/Interface.java
+++ QueryProcessor/src/Interface.java
@@ -5,21 +5,26 @@ import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @SuppressWarnings("ALL")
+@WebServlet("/Interface")
 public class Interface extends HttpServlet {
     //Data members
     public String userInput = "messi";
+    public String checkBox = "check";
     static private MongoDatabase Yara;
     private static MongoClient mongoClient;
-
+    public  String Path = "F:/Tech/CUFE_CHS/Spring 2020/Advance Programming Techniques/Project/Web_Search_Engine/Server/apache-tomcat-8.5.55/webapps/ROOT";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException
     {
@@ -30,6 +35,7 @@ public class Interface extends HttpServlet {
 
         //1.To extract the text entered by the user
         userInput = request.getParameter("textbox");
+        checkBox = request.getParameter("checkbox");
 
         //2. class constructors
         queryProcessor qp = new queryProcessor();
@@ -40,57 +46,69 @@ public class Interface extends HttpServlet {
 
 
         //4.Stem the input query and put in a list
-        KeywordsExtractor keywordsExtractor = new KeywordsExtractor();
-        List<Keyword> keywordList = keywordsExtractor.getKeywordsList(userInput);
+        if(userInput!=null) {
+
+            KeywordsExtractor keywordsExtractor = new KeywordsExtractor();
+            List<Keyword> keywordList = keywordsExtractor.getKeywordsList(userInput);
 
 
-        String [] finalStemmedArray = new String[keywordList.size()];  //Construct a string to put in it the stemmed words
+            String[] finalStemmedArray = new String[keywordList.size()];  //Construct a string to put in it the stemmed words
 
-        for (int i = 0 ; i<keywordList.size();i++){
-            finalStemmedArray[i] = keywordList.get(i).getStem();     //add each stem inside single quotes and concatinate all
-        }
+            for (int i = 0; i < keywordList.size(); i++) {
+                finalStemmedArray[i] = keywordList.get(i).getStem();     //add each stem inside single quotes and concatinate all
+            }
 
-        //5. check  phrase or non phrase then process accordingly (Send data to query processor)
-        if(qp.phraseOrNonphrase(userInput) == 1)
-            docs = qp.nonPhraseSearch(finalStemmedArray,keywordList.size(), collection); //Fills array list toRanker
-        if(qp.phraseOrNonphrase(userInput)== 0)
-            docs = qp.PhraseSearch(finalStemmedArray,keywordList.size(),collection); //Fills array List toRanker
+            //5. check  phrase or non phrase then process accordingly (Send data to query processor)
+            if (checkBox != null && checkBox.equals("ImageSearch")){
+                docs = qp.imageSearch(finalStemmedArray, keywordList.size(), collection);
+            }
+            else if (qp.phraseOrNonphrase(userInput) == 1) {
+                docs = qp.nonPhraseSearch(finalStemmedArray, keywordList.size(), collection);
+            }
+            else if (qp.phraseOrNonphrase(userInput) == 0) {
+                docs = qp.PhraseSearch(finalStemmedArray, keywordList.size(), collection); //Fills array List toRanker
+            }
 
+            //6. Call the ranker ; the toRanker arrayList should be filled by this stage
+            RelevanceRanker rr = new RelevanceRanker();
+            rr.calculateRelevanceRank(docs);
 
-        //6. Call the ranker ; the toRanker arrayList should be filled by this stage
-        RelevanceRanker rr = new RelevanceRanker();
-        System.out.println("Before Relevance Ranker: ");
-        for (DocumentWordEntry doc: docs){
-            System.out.println(doc.getTotalRank());
-        }
-        rr.calculateRelevanceRank(docs);
-        System.out.println("After Relevance Ranker: ");
-        for (DocumentWordEntry doc: docs){
-            System.out.println(doc.getTotalRank());
         }
 
         //7.  print the results on screen
-
         //-set page content
         response.setContentType("text/html");
-        //-Design the page
-
-        //PrintWriter out = response.getWriter();
-        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-        docType = docType +
-                "<html>\n" +
-                "<head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></head>\n" +
-                "<body bgcolor=\"#f0f0f0\">\n" ;
-        for(int i=0;i<docs.size();i++)
-        {
-
-                    docType += "<b><a href="+ docs.get(i).getUrl()+">" +"<font size=\"5\">"+ docs.get(i).getTitle()+"</font>"+"</a></b><br>"+"<font color=\"green\">\n"+ docs.get(i).getUrl()+"</font><br>\n";
-            docType += docs.get(i).getFirstStatement()+"<br>";
-
+        if(checkBox == null) {
+            queryProcessor.createJSFile(docs, Path);
+            response.sendRedirect("http://localhost:8080/result.html");
         }
-        docType +="</body></html>";
-        //- Dispaly page's content
-        response.getWriter().print(docType);
-    }
+        else {
 
+            Set<String> images = new HashSet<String>();
+
+            for (int i = 0; i < docs.size(); i++) {
+                for (int j = 0; j < docs.get(i).getImgSrc().size(); j++) {
+
+
+                    images.add(docs.get(i).getImgSrc().get(j));
+
+
+                }
+            }
+            String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+            docType = docType +
+                    "<html>\n" +
+                    "<head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></head>\n";
+
+            for (String s : images) {
+
+                docType += "<img src=\"" + s + "\" style=\"width:15%\" style=\"height:15%\">";
+
+
+            }
+
+            docType += "</body></html>";
+            response.getWriter().print(docType);
+        }
+    }
 }
--- QueryProcessor/src/queryProcessor.java
+++ QueryProcessor/src/queryProcessor.java
@@ -6,6 +6,9 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import opennlp.tools.stemmer.PorterStemmer;
 import com.mongodb.client.MongoDatabase;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -58,6 +61,47 @@ public class queryProcessor {
         return null;
     }
 
+
+    public ArrayList<DocumentWordEntry>  imageSearch(String[] finalStemmedArray, int length, MongoCollection<Document> collection) {
+        ArrayList<DocumentWordEntry> allResults = new ArrayList<DocumentWordEntry>();
+
+        for (int i = 0; i < length; i++) {
+            FindIterable<Document> documents = (FindIterable<Document>) collection.find(Filters.eq("word", finalStemmedArray[i]));
+            for (Document document : documents) {
+                double pageRank = (double) document.get("page_rank");
+                String docUrl = (String) document.get("doc_url");
+                String docTitle = (String) document.get("title");
+                int wordFrequency = (int) document.get("word_frequency");
+                boolean isInTitle = (boolean) document.get("is_in_title");
+                boolean isInHeader = (boolean) document.get("is_in_header");
+                boolean isInUrl = (boolean) document.get("is_in_url");
+                String firstStatement = (String) document.get("first_statement");
+                String word = (String) document.get("word");
+                int docLength = (int) document.get("total_words_count");
+                double tf = (double) document.get("tf");
+                double idf = (double) document.get("idf");
+                double urlLength = (double) document.get("url_length");
+
+                ArrayList<Document> allWebPages = (ArrayList<Document>) document.get("img_srcs"); //has all info need to be parsed
+
+                List<String> imgSrc = new LinkedList<String>();
+                for (Document obj : allWebPages) {
+
+                    imgSrc.add((String) obj.get("img_Src"));
+                }
+
+                allResults.add(new DocumentWordEntry(pageRank, docUrl, docTitle, wordFrequency, isInTitle,
+                        isInHeader, isInUrl, firstStatement, imgSrc, word, docLength, tf, idf, urlLength));
+
+            }
+        }
+
+
+
+        return  allResults;
+    }
+
+
     //4. nonPhraseSearch
     public ArrayList<DocumentWordEntry> nonPhraseSearch(String[] finalStemmedArray, int length, MongoCollection<Document> collection) throws IOException {
         ArrayList<DocumentWordEntry> toRanker = new ArrayList<DocumentWordEntry>();
@@ -109,6 +153,7 @@ public class queryProcessor {
                     }
 
                     allResults.remove(j);
+                    j--;
                 }
 
             }
@@ -119,6 +164,101 @@ public class queryProcessor {
         return allResults;
     }
 
+
+
+     static void createJSFile(ArrayList<DocumentWordEntry> docs,String Path){
+
+
+         try {
+
+             FileWriter myWriter = new FileWriter(Path+"/pagination.js");
+             myWriter.write("var current_page = 1;\n");
+             myWriter.write("var records_per_page = 10;\n");
+             myWriter.write("var objJson = [\n");
+             for(int i = 0 ; i < docs.size(); i++){
+                 if(i== docs.size()-1){
+                     String Stripped = docs.get(i).getFirstStatement().replaceAll("\""," ").replaceAll("\\n|\\\t"," ");
+                     myWriter.write("    { title:\""+docs.get(i).getTitle().replaceAll("\""," ").replaceAll("\\n|\\\t"," ")+"\" ," +
+                             " statement:\""+Stripped.substring(0,(Stripped.length()>140 ? 140:Stripped.length()))+"..."+"\" ," +
+                             " url:\""+docs.get(i).getUrl().replaceAll("\""," ").replaceAll("\\n|\\\t"," ")+"\"}\n");
+                 }
+                 else {
+                     String Stripped = docs.get(i).getFirstStatement().replaceAll("\""," ").replaceAll("\\n|\\\t"," ");
+                     myWriter.write("    { title: \"" + docs.get(i).getTitle().replaceAll("\"", " ").replaceAll("\\n|\\\t"," ") + "\" ," +
+                             " statement:\""+Stripped.substring(0,(Stripped.length()>140 ? 140:Stripped.length()))+"..."+"\" ," +
+                             " url:\"" + docs.get(i).getUrl().replaceAll("\""," ").replaceAll("\\n|\\\t"," ") + "\"},\n");
+                 }
+             }
+             myWriter.write("];");
+
+             myWriter.write("function prevPage()\n" +
+                     "{\n" +
+                     "    if (current_page > 1) {\n" +
+                     "        current_page--;\n" +
+                     "        changePage(current_page);\n" +
+                     "    }\n" +
+                     "}");
+
+             myWriter.write("function nextPage()\n" +
+                     "{\n" +
+                     "    if (current_page < numPages()) {\n" +
+                     "        current_page++;\n" +
+                     "        changePage(current_page);\n" +
+                     "    }\n" +
+                     "}");
+
+             myWriter.write("function changePage(page)\n" +
+                     "{\n" +
+                     "    var btn_next = document.getElementById(\"btn_next\");\n" +
+                     "    var btn_prev = document.getElementById(\"btn_prev\");\n" +
+                     "    var listing_table = document.getElementById(\"listingTable\");\n" +
+                     "    var page_span = document.getElementById(\"page\");\n" +
+                     "\n" +
+                     "    // Validate page\n" +
+                     "    if (page < 1) page = 1;\n" +
+                     "    if (page > numPages()) page = numPages();\n" +
+                     "\n" +
+                     "    listing_table.innerHTML = \"\";\n" +
+                     "\n" +
+                     "    for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {\n" +
+                     "        listing_table.innerHTML += \"<b><a href=\\\"\"+objJson[i].url+\"\\\"><font size=\\\"5\\\">\"+objJson[i].title+\"</font></a>\"+ \"</b><br>\";\n" +
+                     "        listing_table.innerHTML += \"<font color=\\\"green\\\">\"+objJson[i].url+\"</font><br>\";\n" +
+                     "        listing_table.innerHTML += \"<font color=\\\"black\\\" size=\\\"3\\\">\"+objJson[i].statement+\"</font><br>\"+ \"<br>\";\n" +
+
+                     "    }\n" +
+                     "    page_span.innerHTML = page;\n" +
+                     "\n" +
+                     "    if (page == 1) {\n" +
+                     "        btn_prev.style.visibility = \"hidden\";\n" +
+                     "    } else {\n" +
+                     "        btn_prev.style.visibility = \"visible\";\n" +
+                     "    }\n" +
+                     "\n" +
+                     "    if (page == numPages()) {\n" +
+                     "        btn_next.style.visibility = \"hidden\";\n" +
+                     "    } else {\n" +
+                     "        btn_next.style.visibility = \"visible\";\n" +
+                     "    }\n" +
+                     "}\n");
+
+             myWriter.write("function numPages()\n" +
+                    "{\n" +
+                    "    return Math.ceil(objJson.length / records_per_page);\n" +
+                    "}\n" +
+                    "\n" +
+                    "window.onload = function() {\n" +
+                    "    changePage(1);\n" +
+                    "};");
+
+
+             myWriter.close();
+             System.out.println("Successfully wrote to the file.");
+         } catch (IOException e) {
+             System.out.println("An error occurred.");
+             e.printStackTrace();
+         }
+
+    }
 
 
 } //end class
